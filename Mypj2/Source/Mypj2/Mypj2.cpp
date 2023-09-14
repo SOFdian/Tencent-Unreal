a547576ecaac5c40926290c07eeca9ba688c8e02
SOFdian <1462091643@qq.com>
2023-09-14 16:46:40 +0800

    test

---
+++ Mypj2/Source/Mypj2/Mypj2.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Mypj2.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Mypj2, "Mypj2" );
